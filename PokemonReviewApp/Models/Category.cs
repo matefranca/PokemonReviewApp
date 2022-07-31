7f89a9012c6d86f3b78f256a58ecc19f565dc281
Matheus Waked Franca <matheus363@hotmail.com>
2022-07-31 01:48:26 -0300

    Models Created.

--- PokemonReviewApp/Models/Category.cs
+++ PokemonReviewApp/Models/Category.cs
@@ -1,9 +1,8 @@
 ﻿namespace PokemonReviewApp.Models
 {
-    public class Owner
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Gym { get; set; }
     }
 }
--- PokemonReviewApp/Models/Country.cs
+++ PokemonReviewApp/Models/Country.cs
@@ -1,9 +1,8 @@
 ﻿namespace PokemonReviewApp.Models
 {
-    public class Owner
+    public class Country
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Gym { get; set; }
     }
 }
